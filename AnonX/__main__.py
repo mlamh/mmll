8f5e5e541fac38f2526c4e085b58f3bc17aa7585
Source Freedom <104025850+SoRsRR8r9@users.noreply.github.com>
2022-10-09 05:49:28 +0300

    Update __main__.py

--- AnonX/__main__.py
+++ AnonX/__main__.py
@@ -53,7 +53,7 @@ async def init():
     await Anon.start()
     try:
         await Anon.stream_call(
-            "https://telegra.ph/file/8d5db123638c2f6bb6ce4.mp4"
+            "https://te.legra.ph/file/708ef878b487896990204.mp4"
         )
     except NoActiveGroupCall:
         LOGGER("AnonX").error(
